8de7fd196cdb6f7e1374bff4b9344550c8d5bfe8
Ali Samir <alisamir.2020@azhar.edu.eg>
2022-11-05 23:05:20 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiZGV2YWxpc2FtaXIiLCJhIjoiY2w4eDF5cm40MDI4NjNycG5vY2x6bDV2diJ9._dXVRYRmdrFaWLe1HbEneg";
+
+//   var map = new mapboxgl.Map({
+//   container: "map",
+//   style: "mapbox://styles/mapbox/streets-v11",
+// });
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+  enableHighAccuracy: true,
+});
+
+function successLocation(position) {
+  setupMap([position.coords.longitude, position.coords.latitude]);
+}
+
+function errorLocation() {
+  setupMap([-2.24, 53.48]);
+}
+
+function setupMap(center) {
+  const map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: center,
+    zoom: 15,
+  });
+
+  const nav = new mapboxgl.NavigationControl();
+  map.addControl(nav);
+
+  var directions = new MapboxDirections({
+    accessToken: mapboxgl.accessToken,
+  });
+
+  map.addControl(directions, "top-left");
+}
